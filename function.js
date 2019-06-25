9dfcb8f62761204e34aba816bfaa420daa559e77
ysdln0616 <1419099@ed.tus.ac.jp>
2019-06-25 15:53:27 +0900

    できないよう😭

--- function.js
+++ function.js
@@ -1,20 +1,29 @@
+ var array=[];
+   array.push(1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8);
+ array.sort(function() {
+  return Math.random() - Math.random();
+});
+console.log(array);
 
-  const num=(Math.floor(Math.random () * 8) + 1);
- const b=Math.floor(num);
- console.log("photo/0"+b+".png");
+//arrayの中身を並び替えた
+
+
+//引いた一枚目がarray[0],２枚めがarray[1]...という風にしたい
+//引いたカードを保存したいけど、、、
+//それかA11=array[0],A12=array[1]...
+//select(id)にしてるからできなさそう
+
 
  var flag=-1;
-
-
-function select(){
+ 
+function select(id){
   console.log(flag);
   if(flag==-1){
-    document.getElementById("A11").src="photo/0"+b+".png";
+    document.getElementById(id).src="photo/0"+array[0]+".png";
   }else if(flag==1){
-    document.getElementById("A11").src="photo/00.png";
+    document.getElementById(id).src="photo/00.png";
   }
   flag=flag*-1;
   console.log(flag);
-            }
-
+}
         
